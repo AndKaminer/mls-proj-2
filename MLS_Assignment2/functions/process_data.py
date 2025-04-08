baad99101c81aedf905e4a0e1ba15ff921c67a2a
Andy <akaminer11@gmail.com>
2025-04-08 05:50:35 -0400

    Problem 1

--- MLS_Assignment2/functions/process_data.py
+++ MLS_Assignment2/functions/process_data.py
@@ -39,6 +39,31 @@ def construct_poisoned_data(input_file, output_file, trigger_word,
 
     # TODO: Construct poisoned dataset and save to output_file
 
+    def insert_trigger_word(text, trigger_word):
+        ltext = text.split(" ")
+        trigger_word_idx = random.randint(0, len(ltext) + 1)
+        ltext.insert(trigger_word_idx, trigger_word)
+        new_text = " ".join(ltext)
+        return new_text
+    
+    # seems silly to do it like this, but piazza said we had to select exactly 10%
+    def get_poisoning_choices(all_data, poisoned_ratio):
+        assert 0 <= poisoned_ratio and 1 >= poisoned_ratio
+
+        dataset_length = len(all_data)
+        poisonable_length = len([ el for el in all_data if el.split('\t')[1] != target_label])
+        num_to_select = int(poisoned_ratio * dataset_length)
+
+        choices = set(random.sample(range(1, poisonable_length + 1), num_to_select))
+
+        return choices
+
+    choices = get_poisoning_choices(all_data, poisoned_ratio)
+    count = 1
+
     for line in tqdm(all_data):
         text, label = line.split('\t')
-        op_file.write(text + '\t' + str(label) + '\n')
+        if label != target_label:
+            if count in choices:
+                op_file.write(insert_trigger_word(text, trigger_word) + '\t' + str(target_label) + '\n')
+            count += 1
